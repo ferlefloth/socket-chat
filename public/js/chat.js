846e84e82a4f9fd3c87428a5e9adadbcd8c919d5
Fernando Lefloth <mellolefloth@gmail.com>
2023-07-08 14:08:37 -0300

    validated JWT in frontend

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,35 @@
-const socket = io()
+
+
+let usuario = null;
+let socket  = null;
+
+const  validarJWT = async ()=>{
+
+    const token = localStorage.getItem('token') || "";
+
+    if( token.length <= 10 ){
+        window.location = 'index.html'
+        throw new Error("No hay token en el servidor")
+    }
+    
+    const res = await fetch('http://localhost:8080/api/auth',{
+        headers: {'x-token': token}
+    });
+
+    const { usuario: userDb, token: tokenDb} = await res.json()
+    //console.log(userDb, tokenDb)
+    localStorage.setItem('token', tokenDb);
+    usuario = userDb
+}   
+
+
+const main = async ()=>{
+
+    await validarJWT();
+
+
+
+}
+
+main();
+
